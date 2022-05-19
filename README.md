e4d906cd67ba2a98892cc3dd23b1bf1b448617d7
nathanh635 <nathanh635@gmail.com>
2022-05-19 13:28:13 -0600

    final commit, updated readme and completed verification check in .js file

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 ## Password Generator - Challenge 3
 
-The index.html will take you to a password generator. the generator is designed to generate a random password based on selected inputs, including length, and whether or not to include special, numeric, uppercase, and lowercase characters.
+The index.html will take you to a password generator. The generator is designed to generate a random password based on selected inputs, including length, and whether or not to include special, numeric, uppercase, and lowercase characters.
 
-The password can be anywhere from 8 to 128 characters in length, and must include at least one selected data type.
+The password can be anywhere from 8 to 128 characters in length, and must include at least one selected data type. If no length is selected or if the cancel button is pressed during the length prompt, the function will quit. 
 
+If the provided length is not valid (i.e., outside the range of 8-128), the program will ask the user for an appropriate length and restart.
+
+The program then requests the user confirm to include lower case, upper case, special, and numeric characters.
+
+If no data types are selected, the program will start at the length prompt again.
+
+once at least one valid data type is selected, the program will generate a random password including the selected data types.
+
+Once the password is generated, the code will evaluate the password to verify that it contains the required data types (at least one of each selected type).
+If it does not contain at least one of each selected type, the program will generate a new random password.
+
+![Website viewed on desktop](./assets/images/screenshot.PNG)
+
+Github Repository link: https://github.com/nathanh635/Challenge3-PasswordGenerator
+
+Deployed Website: https://nathanh635.github.io/Challenge3-PasswordGenerator/
--- script.js
+++ script.js
@@ -12,18 +12,9 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//Add prompt for length of password
-
-//Add prompt to confirm special characters
-//Use if loop to include special characters
-//Add prompt to confirm numeric characters
-//Add prompt for lowercase and uppercase
-//confirm that password contains all designated types
-
 function generatePassword() {
 
-
+//Prompt for length of password
   let length = window.prompt("Enter the length of the password (between 8 and 128 characters):");
 if (!length) {
     // If user pressed Cancel, immediately end function
@@ -33,20 +24,21 @@ else if (length > 128 || length < 8) {
   alert("Please enter a number within the range of 8-128 characters.")
   generatePassword()
 }
- 
-
-
-
-
+ // Prompt whether to include lowercase, uppercase, special, and numeric characters and save responses as boolean values
 let lowerCase = window.confirm("Do you want to include lowercase characters?"); 
 let upperCase = window.confirm("Do you want to include uppercase characters?");
 let numeric = window.confirm("Do you want to include numeric characters?"); 
 let special = window.confirm("Do you want to include special characters?"); 
+
+
 let characters = "";
 let totalChars = 0;
 let charnumber = 0;
 let numericChars = "", lowerChars = "", upperChars = "";
+let specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 let num = 0, up = 0, low = 0, spec = 0;
+
+//Depending which data types were selected, construct master string of all possible characters for password selection
 
 if (lowerCase==true) {
   for (let j = 97; j < 123; j++) {
@@ -54,7 +46,6 @@ if (lowerCase==true) {
     lowerChars += String.fromCharCode(j);
         totalChars +=1
     } 
-
 }
 
 if (upperCase==true) {
@@ -63,7 +54,6 @@ if (upperCase==true) {
   upperChars += String.fromCharCode(k);
   totalChars +=1
   } 
-
 }
 
 if (numeric==true) {
@@ -72,44 +62,108 @@ if (numeric==true) {
     numericChars += String.fromCharCode(l);
     totalChars +=1
     } 
-
 }
 
+
 if (special==true) {
-  let specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   totalChars += specialChars.length;
   characters +=specialChars
 }
+
+// If they user clicks cancel for all data types, kick them back to the start of the function
 
 if (lowerCase==false && upperCase == false && numeric == false && special == false) {
   alert("Please select at least one valid data type.")
   generatePassword()
 }
 
+//Define and generate password function
 let pass = "";
 
-function randomPass() {}
-for (let i = 0; i < length; i++) {
+randomPass();
+
+function randomPass() {
+  //reset variables in case button is cliecked multiple times without reloading page, or for when function calls itself
+  pass = ""
+  num = 0, up = 0, low = 0, spec = 0
+
+  // Generate random password from compiled potential string of characters
+  for (let i = 0; i < length; i++) {
   charNumber = Math.floor(Math.random()*totalChars);
   pass += characters.charAt(charNumber);
 }
+
+//confirm that password contains all designated types by checking all individual strings against password and ensure at least one of each selected type is included
+//will look through password and count number of that character type in password string
+
+ if (lowerCase == true) {
+  for (let i = 0; i<pass.length; i++) {
+        for (let j = 0; j<lowerChars.length; j++) {
+          if (pass.charAt(i) === lowerChars.charAt(j)) {
+            low+=1;
+          }
+        }
+  }
 }
 
-if (lowerCase == true) {
-
-
-}
-else if {numeric == true) {
-
-}
-else if {upperCase == true) {
-
-}
-else if {special == true) {
-
+if (upperCase == true) {
+  for (let i = 0; i<pass.length; i++) {
+        for (let j = 0; j<upperChars.length; j++) {
+          if (pass.charAt(i) === upperChars.charAt(j)) {
+            up+=1;
+          }
+        }
+  }
 }
 
+if (numeric == true) {
+  for (let i = 0; i<pass.length; i++) {
+        for (let j = 0; j<numericChars.length; j++) {
+          if (pass.charAt(i) === numericChars.charAt(j)) {
+            num+=1;
+          }
+        }
+  }
+}
 
+if (special == true) {
+  for (let i = 0; i<pass.length; i++) {
+        for (let j = 0; j<specialChars.length; j++) {
+          if (pass.charAt(i) === specialChars.charAt(j)) {
+            spec+=1;
+          }
+        }
+  }
+}
+
+//confirm that at least one of the selected password characters are present, or restart the function
+
+if (numeric == true && num > 0) {
+} else if (numeric == false) {
+} else {
+      randomPass();
+}
+
+if (upperCase == true && up > 0) {
+} else if (upperCase == false) {
+} else {
+      randomPass();
+}
+
+if (lowerCase == true && low > 0) {
+} else if (lowerCase == false) {
+} else {
+      randomPass();
+}
+
+if (special == true && spec > 0) {
+} else if (special == false) {
+} else {
+      randomPass();
+}
+
+} 
+//return password to function above
 return pass;
 }
 
